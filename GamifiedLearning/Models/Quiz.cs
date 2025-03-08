6235de62a23905dbca676cba317a8b08a686b881
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2025-03-08 12:15:54 +0100

    Initial models

---
+++ GamifiedLearning/Models/Quiz.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamifiedLearning.Models
+{
+    public class Quiz
+    {
+        public int QuizId { get; set; }
+        public int LessonId { get; set; }
+        public Lesson Lesson { get; set; }
+
+        [Required]
+        public string Question { get; set; }
+
+        [Required]
+        public string Answer { get; set; }
+
+        public int Points { get; set; }
+    }
+}
---
+++ GamifiedLearning/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamifiedLearning.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public int TotalPoints { get; set; }
+        public int Level { get; set; }
+
+        public string Badges { get; set; }
+    }
+}
